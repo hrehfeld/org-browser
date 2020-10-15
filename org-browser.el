bea4a49c1ef29338814697e7e2cf0bcb4539e6e7
Hauke Rehfeld <git@haukerehfeld.de>
2020-10-15 14:10:31 +0200

    add: more syncing

--- org-browser.el
+++ org-browser.el
@@ -2,7 +2,6 @@
 (require 'org-ml)
 (require 'async)
 (require 'dash)
-(require 'request)
 (require 'bindat)
 (require 'uuid)
 
@@ -13,6 +12,7 @@
 
 `org-agenda-files` might be a good choice.")
 ;;(setq org-browser-sync-files org-agenda-files)
+;;(setq org-browser-sync-files (list org-browser-inbox-file))
 
 (defvar org-browser-url-property-name "URL" "Name of the property that is used to associate URLs to headlines.")
 (defvar org-browser-open-tab-name "opentab" "Marker to identify headlines that have an open tab in the browser.
@@ -37,50 +37,79 @@ If a string mark the headline with a property of that name. The property value w
   (format "http://%s:%s/%s" org-browser-connection-host org-browser-connection-port what))
 
 
+(defvar org-browser-connection-buffer (generate-new-buffer "org-browser-connection-input") "Buffer messages until msg has arrived completely")
+
+
+;; TODO: rename internal
+(defvar org-browser-current-message-size nil)
+
+(defun org-browser--message-id (msg)
+  (gethash "imessage" msg))
+
+(defun org-browser--message-payload (msg)
+  (gethash "payload" msg))
+
 (defun org-browser-connection-filter (proc raw)
   ;;(message (format  "received: %s (%s)" raw (type-of raw)))
-  (let* ((raw (decode-coding-string raw 'binary))
-		 (msg-size (org-browser--unpack-u32 raw))
-		 (raw (substring raw 4))
-		 (msg (encode-coding-string raw 'utf-8))
-		 )
-	;;(message "%s: %s" msg-size msg)
-	(let* ((msg (json-read-from-string msg))
-		   (message-id (cdr (assq 'imessage msg)))
-		   (payload (cdr (assq 'payload msg)))
-		   (handler (cdr (assoc message-id org-browser--response-handlers))))
-	  (setq org-browser--response-handlers (assoc-delete-all message-id org-browser--response-handlers))
-	  (message "Received response: %s" message-id)
-	  (funcall handler payload)
-	  )
-	)
-  )
+  (let* ((proc-buffer (process-buffer org-browser--connection)))
+	(when (= org-browser-current-message-size 0)
+	  ;;(raw (encode-coding-string raw 'binary))
+	  (setq org-browser-current-message-size (org-browser--unpack-u32 raw))
+	  ;; ignore/cut-off msg-size bytes
+	  (setq raw (substring-no-properties raw 4)))
+	(with-current-buffer proc-buffer
+	  (insert raw)
+	  ;;(message "%s vs. %s" (buffer-size) org-browser-current-message-size)
+	  (when (>= (buffer-size) org-browser-current-message-size)
+		(let* ((temp-buffer (get-buffer-create (make-temp-name "*org-browser-message-")))
+			   (message-end (1+ org-browser-current-message-size))
+			   (serialized (decode-coding-region (point-min) message-end 'utf-8 temp-buffer)))
+		  (delete-region (point-min) message-end)
+		  (setq org-browser-current-message-size 0)
+		  (with-current-buffer temp-buffer
+			(let* ((msg (json-parse-buffer))
+				   (message-id (org-browser--message-id msg))
+				   (payload (org-browser--message-payload msg))
+				   (handler (cdr (assoc message-id org-browser--response-handlers))))
+			  ;;(message "%S: %S" message-id msg)
+			  (assert handler)
+			  (setq org-browser--response-handlers (assoc-delete-all message-id org-browser--response-handlers))
+			  (message "Received response: %s" message-id)
+			  (funcall handler payload)
+			  ))
+		  (kill-buffer temp-buffer))))))
 
 
 (defun org-browser-connection-sentinel (proc msg)
   (message "Sentinel: %s" msg )
   (cond
-   ((string= msg "connection broken by remote peer\n")
+   ((member msg '("connection broken by remote peer\n" "deleted\n"))
 	(org-browser-connection-delete))
    (t (message (format "=====UNKNOWN sentinel message: %s" msg))
 	  (debug)
 	  (org-browser-connection-delete))))
 
 (defun org-browser-connection-open ()
+  (org-browser-connection-delete)
   (let* ((name (make-temp-name "org-browser-connection-"))
 		 (proc-buffer (get-buffer-create (concat "*" name "*")))
 		 (proc (make-network-process :name name :buffer proc-buffer
 									 :host org-browser-connection-host :service org-browser-connection-port
 									 ;;:coding 'binary
 									 )))
-	;;(set-process-filter-multibyte proc t)
-	(set-process-coding-system proc 'utf-8 'utf-8)
-	;;(set-process-coding-system proc 'binary 'binary)
+	(set-process-filter-multibyte proc nil)
+	;;(set-process-coding-system proc 'utf-8 'utf-8)
+	(set-process-coding-system proc 'binary 'binary)
 	(set-process-sentinel proc #'org-browser-connection-sentinel)
 	(set-process-filter proc #'org-browser-connection-filter)
 	(setq org-browser--connection proc)
+	(setq org-browser-current-message-size 0
+		  org-browser--response-handlers nil)
 	proc
 	))
+;;(org-browser-connection-open)
+;;(global-set-key (kbd "<f7>") (lambda () (interactive) (org-browser-connection-open)))
+
 
 (defun org-browser-connection-get ()
   (unless org-browser--connection (org-browser-connection-open))
@@ -89,8 +118,11 @@ If a string mark the headline with a property of that name. The property value w
 
 (defun org-browser-connection-delete ()
   "Low-level send"
-  (delete-process org-browser--connection)
-  (setq org-browser--connection nil))
+  (when org-browser--connection
+	(kill-buffer (process-buffer org-browser--connection))
+	;;(delete-process org-browser--connection)
+	(setq org-browser--connection nil)
+	))
 ;; (org-browser-connection-delete)
 
 (defun org-browser-connection-send-raw (msg)
@@ -106,7 +138,6 @@ If a string mark the headline with a property of that name. The property value w
 (defun org-browser-connection-send (payload handler)
   (let* ((imessage (uuid-string))
 		 (msg `((payload . ,payload) (imessage . ,imessage))))
-	(message "new message: %s" imessage)
 	(setq org-browser--response-handlers
 		  (append (list (cons imessage handler))
 				  org-browser--response-handlers))
@@ -189,31 +220,8 @@ If a string mark the headline with a property of that name. The property value w
 						  (org-ml-update* (org-ml-headline-set-node-property org-browser-url-property-name url it) headline)))
 					  url))))
 	  (cl-flet ((handle-url-found (tab)
-								  (let* ((headline (org-browser-filter-headline-with-uuid headline-id (org-ml-parse-this-buffer)))
-										 (headline-title (save-excursion (org-util-headline-get-title headline)))
-										 (title (org-browser-tab-title tab))
-										 (title (when (not (string-equal title headline-title))
-												  (let ((read-answer-short t)
-														(action (read-answer (format "Overwrite Headline Title? \"%s\" -> \"%s\": " headline-title title)
-																			 '(("yes"  ?y "overwrite with tab title")
-																			   ("no"   ?n "keep org title")
-																			   ("edit"  ?e "edit tab title and overwrite")))))
-													(xcond
-													 ((string-equal action "edit")
-													  (read-string "Edit new title: " title))
-													 ((string-equal action "no")
-													  nil)
-													 (t title))))))
-									(save-excursion
-									  (when title
-										(org-ml-update* (org-ml-headline-set-title! title nil it) headline))
-									  (org-ml-update*
-										(if (eq org-browser-open-tab-type 'tag)
-											(org-ml-insert-into-property :tags 0 org-browser-open-tab-name it)
-										  (org-ml-headline-set-node-property
-										   org-browser-open-tab-type
-										   org-browser-open-tab-name it))
-										headline)))))
+								  (let* ((headline (org-browser-filter-headline-with-uuid headline-id (org-ml-parse-this-buffer))))
+									(org-browser-update-headline tab headline))))
 		(if url
 			(org-browser-url-is-opened
 			 (url-normalize-url url)
@@ -227,5 +235,99 @@ If a string mark the headline with a property of that name. The property value w
 				 )))
 		  (error "No URL on this headline"))))))
 ;;(global-set-key (kbd "<f9>") #'org-browser-sync-headline)
+
+(defun org-browser-headline-title-escape (title)
+  (replace-regexp-in-string "[:*]+" "" title))
+
+(defun org-browser-headline-update (tab headline)
+  (let* ((headline-title (org-util-headline-get-title headline))
+		 (title (org-browser-tab-title tab))
+		 (url (org-browser-tab-url tab)))
+	(when (and (not (string-empty-p headline-title))
+			   (not (string-equal title headline-title)))
+	  (setq title
+			(let ((read-answer-short t)
+				  (action (read-answer (format "Overwrite Headline Title? \"%s\" -> \"%s\": " headline-title title)
+									   '(("yes"  ?y "overwrite with tab title")
+										 ("no"   ?n "keep org title")
+										 ("edit"  ?e "edit tab title and overwrite")))))
+			  (xcond
+			   ((string-equal action "edit")
+				(read-string "Edit new title: " title))
+			   ((string-equal action "no")
+				nil)
+			   (t title)))))
+	(setq headline
+		  (org-ml-headline-set-node-property org-browser-url-property-name url headline))
+	(when title
+	  (setq headline (org-ml-headline-set-title! title nil headline)))
+	(if (eq org-browser-open-tab-type 'tag)
+		(org-ml-insert-into-property :tags 0 org-browser-open-tab-name headline)
+	  (org-ml-headline-set-node-property
+	   org-browser-open-tab-type
+	   org-browser-open-tab-name headline))))
+
+(defun org-browser-update-headline (tab headline)
+  (org-ml-update #'identity (org-browser-headline-update tab headline)))
+
+(defun org-browser-get-items  (callback)
+  (org-browser-connection-send
+   `((type . all))
+   callback))
+
+(defun org-browser-tab-url (tab)
+  (gethash "url" tab))
+
+(defun org-browser-tab-title (tab)
+  (gethash "title" tab))
+
+(defun org-browser-headline-url (headline)
+  (org-ml-headline-get-node-property org-browser-url-property-name headline))
+
+(defun org-browser-sync ()
+  (interactive)
+  (org-browser-get-items
+   (lambda (item-list)
+	 (message "%s tabs received" (length item-list))
+	 ;;(message "%S" item-list)
+	 (let ((tabs-map (make-hash-table :test 'equal))
+		   (found-tabs (make-hash-table :test 'equal)))
+	   (seq-doseq (tab item-list)
+		 (puthash (url-normalize-url (org-browser-tab-url tab)) tab tabs-map)
+		 )
+
+	   (let ((sync-files (append org-browser-sync-files
+								 (list org-browser-inbox-file))))
+		 (save-window-excursion
+		   (save-excursion
+			 (dolist (file-name sync-files)
+			   ;;(message "Inspecting %s" file-name)
+			   (with-current-buffer (find-file file-name)
+				 (save-excursion
+				   (let ((matching-headlines
+						  (->> (org-ml-parse-this-buffer)
+							   (org-ml-match '(headline))
+							   (--filter (gethash (url-normalize-url (org-browser-headline-url it)) tabs-map)))))
+					 (dolist (headline matching-headlines)
+					   (let* ((url (org-browser-headline-url headline))
+										(message "Found headline for %s in %s" url file-name)
+							 (tab (gethash url tabs-map)))
+						 (puthash url nil found-tabs)
+						 (org-browser-update-headline tab headline)))
+					 )))
+			   ))))
+	   (with-current-buffer (find-file org-browser-inbox-file)
+		 (save-excursion
+		   (maphash (lambda (url tab)
+					  (unless (gethash url found-tabs)
+						(org-ml-insert-tail (point-max)
+																(org-browser-headline-update tab (org-ml-build-headline)))
+						))
+					tabs-map)))
+	   )
+	 )))
+;;(global-set-key (kbd "<f8>") #'org-browser-sync)
+;;(setq print-length 999)
+
 
 (provide 'org-browser)
---
+++ org-util.el
@@ -0,0 +1,36 @@
+(require 'org-ml)
+
+
+(defun org-util-list-choice-prompt (candidates prompt)
+  "Select one choice from CANDIDATES.
+
+If CANDIDATES is empty, return nil.
+If CANDIDATES has one element, return that element.
+Otherwise PROMPT the user for a choice."
+  (when candidates
+	  (cond
+	 ((not (cdr candidates))
+	  (car candidates))
+	 (t
+	  (completing-read prompt candidates nil t (car candidates))))))
+
+(defun org-util-headline-get-title (headline)
+  (substring-no-properties (or (car (org-ml-get-property :title headline)) "")))
+
+(defun org-util-headline-get-links (headline)
+  "Find all links in HEADLINE and return as list."
+  (let* ((section (org-ml-headline-get-contents t t headline))
+		(title (org-ml-get-property :title headline))
+		(link-lists (--map (org-ml-match '(link) it) (cons (org-ml-get-children title) section)))
+		(links (apply #'append link-lists)))
+	links))
+
+(defun org-util-headline-get-urls (headline)
+  "Find all http(s) links in HEADLINE and return as list."
+  (let ((valid-link-types '("http" "https")))
+	(--map (org-ml-get-property :raw-link it)
+		   (--filter (member (org-ml-get-property :type it) valid-link-types)
+					 (org-util-headline-get-links headline)))))
+
+
+(provide 'org-util)
